c2825e00740696ccd7c4ce76959b0d0a8ede36f6
HousienNaserMho <aldhahabimho@gmail.com>
2022-04-22 23:21:41 +0300

    The whole project

---
+++ cypress/integration/25_as.spec.js
@@ -0,0 +1,9 @@
+describe('aliase', () => {
+  it('Aliases in Cypress', () => {
+    cy.get('@url').should('contain', 'index.html');
+  });
+
+  it('Aliases in Cypress', () => {
+    cy.get('@url').should('contain', 'index.html');
+  });
+});
